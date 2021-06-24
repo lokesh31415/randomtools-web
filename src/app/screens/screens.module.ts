1d4f6605059cf264e00eb9224ea5311007ede828
loki-dslkhs <storageforprojectfiles@gmail.com>
2021-06-24 06:49:04 +0530

    main page

--- src/app/screens/screens.module.ts
+++ src/app/screens/screens.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '../app-routing.module';
 import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
@@ -6,7 +7,7 @@ import { HomeComponent } from './components';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [SharedModule, CoreModule, AppRoutingModule],
+  imports: [SharedModule, CoreModule, AppRoutingModule, BrowserModule],
   exports: [HomeComponent],
 })
 export class ScreensModule {}
